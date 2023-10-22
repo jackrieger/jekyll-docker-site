5a18e0f7efc7ab2d89f5e07f34f6ada65d3b6fa9
Jack Rieger <jack@jackrieger.com>
2023-10-22 16:43:19 -0400

    Finally working setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,13 @@
 # "#################################################"
 
 # Allow all permissions for this folder, but skip the git files
-# because changing permissions there causes errors
+# because changing permissions there causes errors.
+#
+# NOTE: take out the .git folder that comes with this repo
+# for this step, and then add it back in afterwards. Changing
+# permissions on git files leads to a misleading error that
+# makes it seem like the build did not complete when it actually
+# is fine.
 chmod -R 777 .
 chown -R root:root .
 
@@ -14,3 +20,12 @@ bundle install
 
 # Create Jekyll site
 bundle exec jekyll new . --force --skip-bundle
+
+# NOTE: Jekyll has now overriden the original Gemfile
+# needed to do the initial build.
+
+# Configure Jekyll for GitHub Pages
+bundle add "github-pages" --group "jekyll_plugins" --version 228
+
+# Add webrick dependency removed by Ruby but needed by Jekyll
+bundle add webrick
